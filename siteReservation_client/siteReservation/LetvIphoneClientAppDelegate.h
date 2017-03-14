e6a31a67c7ac5043a7e738f85d9019e4aa9bc985
Nigel Lee <lihui8@le.com>
2017-03-14 16:51:29 +0800

    增加人物素材图片

---
+++ siteReservation_client/siteReservation/LetvIphoneClientAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LetvIphoneClientAppDelegate.h
+//  siteReservation
+//
+//  Created by Nigel Lee on 14/03/2017.
+//  Copyright © 2017 Apress. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface LetvIphoneClientAppDelegate : AppDelegate
+
+@end
---
+++ siteReservation_client/siteReservation/LetvIphoneClientAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  LetvIphoneClientAppDelegate.m
+//  siteReservation
+//
+//  Created by Nigel Lee on 14/03/2017.
+//  Copyright © 2017 Apress. All rights reserved.
+//
+
+#import "LetvIphoneClientAppDelegate.h"
+
+@implementation LetvIphoneClientAppDelegate
+
+@end
