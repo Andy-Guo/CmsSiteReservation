7c136a6f97e049275d86ba575ab8b34f52cca45b
Nigel Lee <lihui8@le.com>
2017-03-16 20:58:04 +0800

    增加LoginVC的无密码登陆的category

---
+++ siteReservation_client/siteReservation/Login/LoginViewController+NoPassword.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController+NoPassword.h
+//  siteReservation
+//
+//  Created by Nigel Lee on 16/03/2017.
+//  Copyright © 2017 Apress. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController (NoPassword)
+
+@end
---
+++ siteReservation_client/siteReservation/Login/LoginViewController+NoPassword.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController+NoPassword.m
+//  siteReservation
+//
+//  Created by Nigel Lee on 16/03/2017.
+//  Copyright © 2017 Apress. All rights reserved.
+//
+
+#import "LoginViewController+NoPassword.h"
+
+@implementation LoginViewController (NoPassword)
+
+@end
